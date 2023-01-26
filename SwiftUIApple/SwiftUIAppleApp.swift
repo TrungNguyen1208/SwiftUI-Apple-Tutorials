002e505bcc5d79bc887ae231b02c9bf835cdb485
Evans Nguyen <evans.trungnguyen@setel.com>
2023-01-26 20:50:25 +0700

    Initial Commit

---
+++ SwiftUIApple/SwiftUIAppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppleApp.swift
+//  SwiftUIApple
+//
+//  Created by Trung Nguyen on 26/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
